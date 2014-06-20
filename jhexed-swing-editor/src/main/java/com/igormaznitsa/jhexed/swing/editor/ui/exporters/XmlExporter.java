0297e6ff5684038cbbba7d933a949c0e1da9f97a
rrg4400 <rrg4400@gmail.com>
2014-06-20 22:58:21 +0300

    added license header into files

--- jhexed-swing-editor/src/main/java/com/igormaznitsa/jhexed/swing/editor/ui/exporters/XmlExporter.java
+++ jhexed-swing-editor/src/main/java/com/igormaznitsa/jhexed/swing/editor/ui/exporters/XmlExporter.java
@@ -1,5 +1,5 @@
-/*
- * Copyright 2014 Igor Maznitsa.
+/* 
+ * Copyright 2014 Igor Maznitsa (http://www.igormaznitsa.com).
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.igormaznitsa.jhexed.swing.editor.ui.exporters;
 
 import com.igormaznitsa.jhexed.engine.HexEngine;
